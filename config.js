7b988cdd2d50e4822bfa0007abdef3e1692cc47f
Pamidukeshara123 <145844894+Pamidukeshara123@users.noreply.github.com>
2024-09-30 20:41:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 
 module.exports = {
   PREFIX: ".", //set Prefix
-  OWNER: "94775200935",//Enter Owner Number
-  USER_NAME: "****",//Enter Your UserName (contact Darkwinzo for get username and password +94775200935)
-  PASSWORD: "********",//Enter Your Password
-  GROUP_JID: ["*******"],//News Group Jid
+  OWNER: "94704020146",//Enter Owner Number
+  USER_NAME: "NIKO TECH PAWIDU",//Enter Your UserName (contact Darkwinzo for get username and password +94775200935)
+  PASSWORD: "123456",//Enter Your Password
+  GROUP_JID: ["120363317542947574@g.us"],//News Group Jid
 };
 
